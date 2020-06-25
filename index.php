47ee5d804aba034387485fb8feb97df027368eea
Zuhaila moideen <zuhailamoideen14@gmail.com>
2020-06-25 21:00:35 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<DOCTYPE html>
+	<!DOCTYPE html>
+	<html>
+	<head>
+		 <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>index1</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1"  >
+    <link rel='stylesheet' type='text/css' href='main.css' media='screen'>
+    <script src='main.js'></script>
+</head>
+<body>
+<br>
+<br>
+
+<form name="form1" action="view.php" method="POST" autocomplete="off">
+
+
+<input type="text" name="name"  placeholder="Enter Your Name Here">
+<input type="submit" name="submit" value="submit">
+
+</form>
+</body>
+</html>
+
